fd4021a5dc9d8d0dcb4baa14335dd60c2fcc072b
wowa <noemail@mxn.com>
2021-05-31 13:12:31 +0200

    Original FFR Software Ported to STM32F407VGt6 Firmware Version 1.26.1

---
+++ E_Kart_testing/Core/Inc/rcp_mode.h
@@ -0,0 +1,13 @@
+/*
+ * rcp_mode.h
+ *
+ *  Created on: 31.05.2021
+ *      Author: rcp_devteam_wk
+ */
+
+#ifndef INC_RCP_MODE_H_
+#define INC_RCP_MODE_H_
+
+
+
+#endif /* INC_RCP_MODE_H_ */
---
+++ E_Kart_testing/Core/Src/rcp_mode.c
@@ -0,0 +1,8 @@
+/*
+ * rcp_mode.c
+ *
+ *  Created on: 31.05.2021
+ *      Author: rcp_devteam_wk
+ */
+
+
